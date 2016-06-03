e9b7060f696881f8e5ec6d0f78e55ef29f1519a0
Wayne1234 <wangyi123wangyi123@outlook.com>
2016-06-03 11:26:00 +0800

    首页优化3

--- js/loadIndexPic.js
+++ js/loadIndexPic.js
@@ -23,8 +23,8 @@ $(document).ready(function () {
         var times = url.split("?"); //分切变量 url 分隔符号为 "?"
         if(times[1] != 1){ //如果?后的值不等于1表示没有刷新
             url += "?1"; //把变量 url 的值加入 ?1
-            self.location.replace(url); //刷新页面
+            self.location.reload(); //刷新页面
         }
     }
-    onload=reurl
+    onload=reurl;
 })
